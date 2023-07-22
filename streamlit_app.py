0b5fdd5a9d6152b63ade58713998989ceb459b95
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2023-07-21 18:26:17 -0700

    🚧 Add first bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,22 +16,12 @@ import streamlit as st
 st.subheader("| Intro")
 st.image("static/R. Alvarado.jpg")
 
-total_points = st.slider("Number of points in spiral", 1, 5432, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-Point = namedtuple('Point', 'x y')
-data = []
-
-points_per_turn = total_points / num_turns
-
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
+if st.session_state.stage >= 2:
+    st.write(f'Hello {name}!')
+    color = st.selectbox(
+        'Pick a Color',
+        [None, 'red', 'orange', 'green', 'blue', 'violet'],
+        on_change=set_state, args=[3]
+    )
+    if color is None:
+        set_state(2)
