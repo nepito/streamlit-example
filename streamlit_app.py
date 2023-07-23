fda92abba7d0a452cbc9a8daa9d3320e3212ea8f
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2023-07-22 22:15:48 -0700

    ⚗️ Try annual weather heatmap

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from vega_datasets import data
+
+source = data.seattle_weather()
 
 """
 # Plataforma Interactiva para Transferencias Óptimas
@@ -25,3 +28,15 @@ player = st.selectbox(
 
 if st.button('Muestra la gráfica de desempeño'):
     st.image(f"static/{player}.jpg")
+
+st.altair_chart(
+    alt.Chart(source, title="Daily Max Temperatures (C) in Seattle, WA").mark_rect().encode(
+        x=alt.X("date(date):O", title="Day", axis=alt.Axis(format="%e", labelAngle=0)),
+        y=alt.Y("month(date):O", title="Month"),
+        color=alt.Color("max(temp_max)", legend=alt.Legend(title=None)),
+        tooltip=[
+            alt.Tooltip("monthdate(date)", title="Date"),
+            alt.Tooltip("max(temp_max)", title="Max Temp"),
+        ],
+    ).configure_view(step=13, strokeWidth=0).configure_axis(domain=False)
+)
