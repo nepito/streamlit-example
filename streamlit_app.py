4166cb380fc78c702e4f8da1395e1122850b814c
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2023-07-31 21:18:41 -0700

    ⚗️ Try consistent plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ with tab2:
 # Crear el gráfico de Altair
     chart = alt.Chart(played_minutes, title="Minutes Played by Player and Match").mark_rect().encode(
         alt.X("match:O", sort=alt.EncodingSortField(field="date", order="ascending")).title("Match"),
-        alt.Y("player:O", sort=alt.EncodingSortField(field="minutes", op="sum", order="descending")).title("Player"),
+        alt.Y("player:O", sort=alt.EncodingSortField(field="minutes", op="sum", order="descending"), title = "Player"),
         alt.Color("minutes:Q", scale=alt.Scale(scheme='blues')).title("Minutes"),
         tooltip=[
             alt.Tooltip("match:O", title="Match"),
