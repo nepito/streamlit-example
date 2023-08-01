ae317f87b84efbfeb351897ba0d58e0bfbd6ca27
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2023-07-31 21:59:49 -0700

    ⚗️ Try remove values axis

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ with tab2:
 
 # Crear el gráfico de Altair
     chart = alt.Chart(played_minutes, title="Minutes Played by Player and Match").mark_rect().encode(
-        alt.X("match:N", sort=alt.EncodingSortField(field="date", order="ascending"), axis=alt.Axis(values = "")).title("Match"),
+        alt.X("match:N", sort=alt.EncodingSortField(field="date", order="ascending"), axis=alt.Axis(values = played_minutes.match.to_list())).title("Match"),
         alt.Y("player:N", sort=alt.EncodingSortField(field="minutes", op="sum", order="descending"), title = "Player"),
         alt.Color("minutes:Q", scale=alt.Scale(scheme='blues')).title("Minutes"),
         tooltip=[
